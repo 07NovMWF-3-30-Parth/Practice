5a09ad916ca6d8d33e3b1540b290c7feccce3a83
07NovMWF-3-30-parth <parthdholakiya93@gmail.com>
2023-04-28 11:05:18 +0530

    activity

--- 003a-simple_javascript/lec10.2_formValidation/script.js
+++
@@ -1,9 +0,0 @@
-function validateform(){
-    var name=document.contactform.name.value;
-    var email=document.contactform.email.value;
-    var mobile=document.contactform.mobile.value;
-    var country=document.contactform.country.value;
-    var gender=document.contactform.gender.value;
-    var hobbies=[];
-    var checkboxes=document.getElementsByName("hobbies[]");
-}
